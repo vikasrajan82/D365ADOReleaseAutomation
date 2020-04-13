b3b1f1bf73e05e59ef177ac1401dec31bfee5022
Vikas Rajan <virajan@microsoft.com>
2020-04-13 15:47:45 +0530

    Included Release folder

---
+++ Deployment/Release/Tasks/MSCRMCopy/Print.ps1
@@ -0,0 +1,18 @@
+﻿Write-Host "Checking123"
+
+Write-Host "$env:SYSTEM_DEFAULTWORKINGDIRECTORY"
+
+$ScriptDir = Split-Path $MyInvocation.MyCommand.Path
+
+#Write-Host $MyInvocation.MyCommand.Path
+
+#Get-Location
+
+#Write-Host "After printing directory path"
+
+#Get-ChildItem -Recurse -Directory
+
+
+$assembly = [Reflection.Assembly]::LoadFile("$ScriptDir\ConsoleLibrary.dll")
+$instance = New-Object ConsoleLibrary.Class1
+$instance.Print()
